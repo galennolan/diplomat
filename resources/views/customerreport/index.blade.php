52398a44f1f503712d7faf1087bb8d17a8a764c3
galennolan <galen.nolan1@gmail.com>
2023-05-15 11:36:38 +0700

    t

--- resources/views/customerreport/index.blade.php
+++ resources/views/customerreport/index.blade.php
@@ -448,27 +448,37 @@
   const rokoklain = {!! $rokoklain !!};
   
   const chart4Data = {
-    labels: rokoklain,
-    datasets: [{
-      label: 'Rokok Merk Lain',
-      data: JMLrokoklain,
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)'
-      ],
-      borderWidth: 1
-    }]
-  };
+  labels: rokoklain,
+  datasets: [{
+    label: 'Rokok Merk Lain',
+    data: JMLrokoklain,
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 206, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 0, 255, 0.2)',
+      'rgba(0, 128, 0, 0.2)',
+      'rgba(128, 0, 128, 0.2)',
+      'rgba(0, 128, 128, 0.2)'
+    ],
+    borderColor: [
+      'rgba(255, 99, 132, 1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 206, 86, 1)',
+      'rgba(75, 192, 192, 1)',
+      'rgba(153, 102, 255, 1)',
+      'rgba(255, 159, 64, 1)',
+      'rgba(255, 0, 255, 1)',
+      'rgba(0, 128, 0, 1)',
+      'rgba(128, 0, 128, 1)',
+      'rgba(0, 128, 128, 1)'
+    ],
+    borderWidth: 1
+  }]
+};
   
 
   const chart4Options = {
